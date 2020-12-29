3fd17788c3f33b56c7bd6a0f42e0e5a476763a9a
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-30 00:04:14 +0100

    Checking if Review already exists on backend
    Check if Review already exists on backend.

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="form-group">
             <label for="content" class="text-muted">Describe your experience with</label>
-            <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
         </div>
         <button class="btn btn-lg btn-primary btn-block">Submit</button>
     </div>
@@ -25,11 +25,11 @@ export default{
                 content: null
             }
         }
+    },
+    created(){
+        //1. If review already exists ( in reviews table by id)
+        //2. Fetch a booking by a review key
+        //3. Store the review
     }
-    /*methods:{
-        onRatingChanged(rating){
-            console.log(rating);
-        }
-    }*/
 }
 </script>
