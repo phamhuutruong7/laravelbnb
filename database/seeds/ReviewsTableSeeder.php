97c238821b08b1d35ae5348cd53e246da1c3f3ff
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2020-12-29 17:38:35 +0100

    Review model factory and seeder
    make Review model factory and seeder. and run migrate.

---
+++ database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Bookable;
+use App\Review;
+
+class ReviewsTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        Bookable::all()->each(function(Bookable $bookable){
+            $reviews = factory(Review::class, random_int(5, 30))->make();
+            $bookable->reviews()->saveMany($reviews);
+        });
+    }
+}
