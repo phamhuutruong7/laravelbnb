0a9d7dabd2ece3b929b70f33ac4885afa0a36759
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-01-01 23:04:59 +0100

    fatalError component
    Make Vue Component to display FatalError.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import VueRouter from  "vue-router";
 import Index from "./Index";
 import moment from "moment";
 import StarRating from "./shared/components/StarRating";
+import FatalError from "./shared/components/FatalError";
 
 window.Vue = require('vue');
 
@@ -13,6 +14,7 @@ Vue.use(VueRouter);
 Vue.filter("fromNow", value => moment(value).fromNow());
 
 Vue.component("star-rating", StarRating);
+Vue.component("fatal-error", FatalError);
 const app = new Vue({
     el: '#app',
     router: router,
