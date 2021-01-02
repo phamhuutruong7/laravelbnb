56ee457fbf23bc5b1b0f842a195a69a2f3277571
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-01-02 01:06:57 +0100

    Use Slot with Vue Component
    Use slot with vue component

--- resources/js/review/Review.vue
+++ resources/js/review/Review.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
+        <success v-if="success">
+            You've left a review, thank you very much
+        </success>
         <fatal-error v-if="error"></fatal-error>
-        <div class="row" v-else>
+        <div class="row" v-if="!success && !error">
             <div :class="[{'col-md-4': twoColumns}, {'d-none': oneColumn}]">
                 <div class="card">
                     <div class="card-body">
@@ -68,38 +71,34 @@ export default {
             loading: false,
             booking: null,
             error: false,
-            sending: false
+            sending: false,
+            success: false
         };
     },
-    created() {
+    async created() {
 
         this.review.id = this.$route.params.id;
         this.loading = true;
         // 1. If review already exists (in reviews table by id)
-        axios
-            .get(`/api/reviews/${this.review.id}`)
-            .then(response => {
-                this.existingReview = response.data.data;
-            })
-            .catch(err => {
-                if (is404(err)) {
-                    // 2. Fetch a booking by a review key
-                    return axios
-                        .get(`/api/booking-by-review/${this.review.id}`)
-                        .then(response => {
-                            this.booking = response.data.data;
-                        })
-                        .catch(err => {
-                            this.error = !is404(err);
 
-                        });
+        try{
+            this.existingReview = (await axios.get(
+                `/api/reviews/${this.review.id}`
+            )).data.data;
+        }catch(err) {
+            if (is404(err)) {
+                try {
+                    this.booking = (await axios.get(
+                        `/api/booking-by-review/${this.review.id}`
+                    )).data.data;
+                } catch (err) {
+                    this.error = !is404(err);
                 }
+            } else {
                 this.error = true;
-            })
-            .then(() => {
-                this.loading = false;
-            });
-
+            }
+        }
+        this.loading = false;
     },
     computed: {
         alreadyReviewed() {
@@ -123,9 +122,13 @@ export default {
             // 3. Store the review
             this.errors = null;
             this.sending = true;
+            this.success = false;
+
             axios
                 .post(`/api/reviews`, this.review)
-                .then(response => console.log(response))
+                .then(response => {
+                    this.success = 201 === response.status;
+                })
                 .catch(err => {
                     if(is422(err)){
                         const errors = err.response.data.errors;
---
+++ resources/js/shared/components/Success.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center success">
+        <i class="fas fa-thumbs-up 5x"></i>
+        <h1 class="mt-4">Success!</h1>
+        <h2>
+            <slot></slot>
+        </h2>
+    </div>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style scoped>
+.success{
+    color: #999;
+    font-size: 10rem;
+}
+</style>
