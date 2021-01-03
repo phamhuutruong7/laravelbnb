fcdd2a9f2fd6591bf8c8bfdc41c7426868fbd7ae
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-01-03 14:52:56 +0100

    Bookable price
    add price for each room in Bookable.

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,14 @@ Route::get('bookables','Api\BookableController@index');
 Route::get('bookables/{id}','Api\BookableController@show');
 
 Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show']);
-Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')->name('bookables.availability.show');
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')
+    ->name('bookables.availability.show');
 
-Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')->name('bookables.reviews.index');
+Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')
+    ->name('bookables.reviews.index');
+
+Route::get('bookables/{bookable}/price', 'Api\BookablePriceController')
+    ->name('bookables.price.show');
+
 Route::get('/booking-by-review/{reviewKey}','Api\BookingByReviewController')->name('booking.by-review.show');
 Route::apiResource('reviews', 'Api\ReviewController')->only(['show', 'store']);
