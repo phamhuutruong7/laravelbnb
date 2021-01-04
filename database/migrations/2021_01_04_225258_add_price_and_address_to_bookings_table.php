162dd9427707e493dcea19954e6d8b8d5c09465a
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2021-01-05 00:00:32 +0100

    Add Address model and change in Booking.

---
+++ database/migrations/2021_01_04_225258_add_price_and_address_to_bookings_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPriceAndAddressToBookingsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->unsignedInteger('price');
+            $table->unsignedBigInteger('address_id')->index()->nullable();
+            $table->foreign('address_id')->references('id')->on('addresses');
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->dropColumn('price');
+            $table->dropForeign(['address_id']);
+            $table->dropColumn('address_id');
+        });
+    }
+}
